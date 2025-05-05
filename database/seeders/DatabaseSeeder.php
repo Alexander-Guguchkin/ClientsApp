3f24962f0f9cdcb8726d42032e82b16b24a53d36
Alexander-Guguchkin <guguckins72@yandex.ru>
2025-05-05 22:04:44 +0300

    Добавлено создание администратора в  seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Database\Seeders\StatusSeeder;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory()->create([
+            'name' => 'Administrator',
+            'email' => 'test@example.com',
+            'password' => Hash::make('AdminPassword')
+        ]);
         $this->call([
             StatusSeeder::class
         ]);
