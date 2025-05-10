e5e8c099b621a176e7adca0b9c7a0b730e945a55
Alexander-Guguchkin <guguckins72@yandex.ru>
2025-05-10 14:52:14 +0300

    переход на tailwindcss

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -3,6 +3,9 @@
         <x-nav-item text="Доска"/>
         <x-nav-item text="Клиенты" href="/clients"/>
         <x-nav-item text="Пользователи" href="/users"/>
+        <div class="bg-gray-100 p-4">
+            <h1">123132</h1>
+        </div>
     </div>
     @auth
         <div class="user-info">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', 'Мой сайт')</title>
     <!-- Styles -->
-    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+    {{-- @vite(['resources/scss/app.scss', 'resources/js/app.js']) --}}
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+    <!-- Styles / Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
 <body>
     <div class="app">
